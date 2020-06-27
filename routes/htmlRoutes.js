d1fd94c6ddaf20c7c6808330f714ea08e9e026b4
Svitlana Zaster <szaster@protonmail.com>
2020-06-26 20:16:07 -0500

    Modified htmlRoutes and apiRoutes files

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+// DEPENDENCIES
+var path = require("path");
+
+// ROUTING
+
+module.exports = function (app) {
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/notes.html"));
+  });
+
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"));
+  });
+};
