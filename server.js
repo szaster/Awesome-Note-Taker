5aee0da8a5f1d965b238c20af61ae7e1479a5995
Svitlana Zaster <szaster@protonmail.com>
2020-06-26 19:48:49 -0500

    Modified server.js file, created htmlRoutes, apiRoutes files

---
+++ server.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+
+// Require dependencies
+var express = require("express");
+
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./assets/js/index.js")(app);
+
+// Start our server so that it can begin listening to client requests.
+// Log (server-side) when our server has started
+server.listen(PORT, function () {
+  console.log("Server listening on PORT" + PORT);
+});
