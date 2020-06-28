54d1de10bc7b63152b136352b129acc12b5665ce
Svitlana Zaster <szaster@protonmail.com>
2020-06-28 14:56:10 -0500

    Displayed content of db.json in the browser

--- db/store.js
+++ db/store.js
@@ -1,23 +1,17 @@
 const fs = require("fs");
 const path = require("path");
-const destination = "../public/notes.html";
+const destination = path.join(__dirname, "db.json");
 
 class Store {
   getAll() {
-    return [];
+    return readDB();
   }
 }
 
-function writeDB(data) {
-  fs.writeFile(destination, data, (err) => {
-    if (err) {
-      console.log(err.message);
-      console.log("The file has not been saved!");
-      process.exit(1);
-    } else {
-      console.log("The file has been saved!");
-    }
-  });
+function readDB() {
+  const buffer = fs.readFileSync(destination);
+  const dbData = JSON.parse(buffer);
+  return dbData;
 }
 
 //Module exports new instance of a class Store
